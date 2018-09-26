56af6f4bc0526a1700fbd074403acae52ca2de85
Matthias Hennig <mhhennig@gmail.com>
2018-09-26 13:46:03 +0100

    Update import list

--- spikeinterface/__init__.py
+++ spikeinterface/__init__.py
@@ -3,4 +3,4 @@ from .OutputExtractor import OutputExtractor
 
 from .extractors.mdaextractors import MdaInputExtractor, MdaOutputExtractor
 from .extractors.mearecextractors import MEArecInputExtractor, MEArecOutputExtractor
-
+from .extractors.biocaminputextractor import BiocamInputExtractor
