2c0d177a5b4bffec3c9089deed3d368d8b5e4635
Alessio Buccino <alejoe9187@gmail.com>
2018-10-16 12:21:20 +0200

    kilosort

--- spikeinterface/__init__.py
+++ spikeinterface/__init__.py
@@ -9,8 +9,10 @@ from .extractors.mdaextractors.mdaextractors import MdaRecordingExtractor, MdaSo
 from .extractors.mearecextractors.mearecextractors import MEArecRecordingExtractor, MEArecSortingExtractor
 from .extractors.biocamrecordingextractor import BiocamRecordingExtractor
 from .extractors.hs2sortingextractor import HS2SortingExtractor
+from .extractors.klustasortingextractor import KlustaSortingExtractor
+from .extractors.kilosortsortingextractor import KilosortSortingExtractor
 from .extractors.numpyextractors.numpyextractors import NumpyRecordingExtractor, NumpySortingExtractor
 from .extractors.nwbextractors.nwbextractors import NwbRecordingExtractor
 from .extractors.openephysextractors.openephysextractors import OpenEphysRecordingExtractor, OpenEphysSortingExtractor
 
-from.probes import *
+from .tools import *
--- spikeinterface/extractors/kilosortsortingextractor/kilosortsortingextractor.py
+++ spikeinterface/extractors/kilosortsortingextractor/kilosortsortingextractor.py
@@ -12,28 +12,16 @@ class KilosortSortingExtractor(SortingExtractor):
         print('Parsing output files...')
         spike_times = np.load(join(kilosort_folder, 'spike_times.npy'))
         spike_clusters = np.load(join(kilosort_folder, 'spike_clusters.npy'))
-        spike_templates = np.load(join(kilosort_folder, 'templates.npy')).swapaxes(1, 2)
-        spike_templates_id = np.load(join(kilosort_folder, 'spike_templates.npy'))
 
         self._spiketrains = []
-        clust_id, n_counts = np.unique(spike_clusters, return_counts=True)
+        clust_id = np.unique(spike_clusters)
         self._unit_ids = list(clust_id)
         spike_times.astype(int)
 
-        counts = 0
-        for clust, count in zip(clust_id, n_counts):
+        self._spiketrais = []
+        for clust in self._unit_ids:
             idx = np.where(spike_clusters == clust)[0]
-            # spike_templates.append(kl_templates[clust])
-            # counts += len(idx)
-            # spike_times = kl_times[idx]
-            # spiketrain = neo.SpikeTrain(spike_times, t_start=t_start, t_stop=t_stop)
-            # spike_trains.append(spiketrain)
-
-        spike_templates = np.array(spike_templates)
-        # set unit properties
-        for i_s, spiketrain in enumerate(self._spiketrains):
-            for key, val in spiketrain.annotations.items():
-                self.setUnitProperty(self.getUnitIds()[i_s], key, val)
+            self._spiketrains.append(spike_times[idx])
 
     def getUnitIds(self):
         return list(self._unit_ids)
@@ -43,6 +31,6 @@ class KilosortSortingExtractor(SortingExtractor):
             start_frame = 0
         if end_frame is None:
             end_frame = np.Inf
-        times = self._spiketrains[self.getUnitIds().index(unit_id)].rescale('s').magnitude * self._fs
+        times = self._spiketrains[self.getUnitIds().index(unit_id)]
         inds = np.where((start_frame <= times) & (times < end_frame))
         return times[inds]
