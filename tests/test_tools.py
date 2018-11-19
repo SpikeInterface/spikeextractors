80d047af0f4f79010c8a0b66e8bd069aaa563933
Cole Hurwitz <colehurwitz@gmail.com>
2018-11-19 11:33:07 +0000

    Update test_tools.py

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -2,7 +2,7 @@ import numpy as np
 import os, sys
 import unittest
 import tempfile, shutil
-import spikeinterface as si
+import spikeextractors as se
 
 
 class TestTools(unittest.TestCase):
@@ -13,32 +13,32 @@ class TestTools(unittest.TestCase):
         X = np.random.normal(0, 1, (M, N))
         self._X = X
         self._samplerate = samplerate
-        self.RX = si.NumpyRecordingExtractor(timeseries=X, samplerate=samplerate)
+        self.RX = se.NumpyRecordingExtractor(timeseries=X, samplerate=samplerate)
         self.test_dir = tempfile.mkdtemp()
 
     def tearDown(self):
         shutil.rmtree(self.test_dir)
 
     def test_load_save_probes(self):
-        SX = si.loadProbeFile(self.RX, 'tests/probe_test.prb')
+        SX = se.loadProbeFile(self.RX, 'tests/probe_test.prb')
         print(SX.getChannelPropertyNames())
         assert 'location' in SX.getChannelPropertyNames()
         assert 'group' in SX.getChannelPropertyNames()
         positions = [SX.getChannelProperty(chan, 'location') for chan in range(self.RX.getNumChannels())]
         # save in csv
-        si.saveProbeFile(SX, self.test_dir + 'geom.csv')
+        se.saveProbeFile(SX, self.test_dir + 'geom.csv')
         # load csv locations
-        SX_load = si.loadProbeFile(SX, self.test_dir + 'geom.csv')
+        SX_load = se.loadProbeFile(SX, self.test_dir + 'geom.csv')
         position_loaded = [SX_load.getChannelProperty(chan, 'location') for chan in range(SX_load.getNumChannels())]
         self.assertTrue(np.allclose(positions[10], position_loaded[10]))
 
     def test_write_dat_file(self):
-        si.writeBinaryDatFormat(self.RX, self.test_dir + 'rec.dat')
+        se.writeBinaryDatFormat(self.RX, self.test_dir + 'rec.dat')
         # load
         data = np.memmap(open(self.test_dir + 'rec.dat'), dtype='float32', mode='r', shape=(self.RX.getNumFrames(),
                                                                                             self.RX.getNumChannels())).T
         assert np.allclose(data, self.RX.getTraces())
-        si.writeBinaryDatFormat(self.RX, self.test_dir + 'rec.dat', transpose=True)
+        se.writeBinaryDatFormat(self.RX, self.test_dir + 'rec.dat', transpose=True)
         # load
         data = np.memmap(open(self.test_dir + 'rec.dat'), dtype='float32', mode='r', shape=(self.RX.getNumChannels(),
                                                                                             self.RX.getNumFrames()))
