11a0f52b11ceeacf9ec54215d6e5ffd2a8f01762
Matthias Hennig <mhhennig@gmail.com>
2019-03-01 22:36:45 +0000

    Minor fixes for compatbility with other extractors

--- spikeextractors/extractors/biocamrecordingextractor/biocamrecordingextractor.py
+++ spikeextractors/extractors/biocamrecordingextractor/biocamrecordingextractor.py
@@ -3,6 +3,7 @@ from spikeextractors import RecordingExtractor
 import numpy as np
 import ctypes
 
+
 def _load_required_modules():
     try:
         import h5py
@@ -20,6 +21,9 @@ class BiocamRecordingExtractor(RecordingExtractor):
             self._recording_file)
         for m in range(self._nRecCh):
             self.setChannelProperty(m, 'location', self._positions[m])
+
+    def __del__(self):
+        self._rf.close()
 
     def getChannelIds(self):
         return list(range(self._nRecCh))
@@ -44,17 +48,31 @@ class BiocamRecordingExtractor(RecordingExtractor):
 
     @staticmethod
     def writeRecording(recording, save_path):
+        # Convert to uV:
+        # AnalogValue = MVOffset + DigitalValue * ADCCountsToMV
+        # Where ADCCountsToMV is defined as:
+        # ADCCountsToMV = SignalInversion * ((MaxVolt - MinVolt) / 2^BitDepth)
+        # And MVOffset as:
+        # MVOffset = SignalInversion * MinVolt
+        # conversion back
+        # DigitalValue = (AnalogValue - MVOffset)/ADCCountsToMV
+        # we center at 2048
+
         h5py = _load_required_modules()
         M = recording.getNumChannels()
         N = recording.getNumFrames()
-        channel_ids = range(M)
         raw = recording.getTraces()
-        if raw.dtype != int:
-            raise Exception('Cannot write dataset in the format with non-int datatype:', raw.dtype)
+        scale = 200
+        data_range = np.percentile(raw,[0,100,50])
+        max_amp = np.max(np.abs(data_range[:2]-data_range[2]))
+        # tt = ((raw-data_range[2])/max_amp*scale).astype(int)+2048
+        # if raw.dtype != int:
+        #     raise Exception('Cannot write dataset in the format with non-int datatype:', raw.dtype)
         rf = h5py.File(save_path, 'w')
         # writing out in 100 format: Time x Channels
         g = rf.create_group('3BData')
-        d = rf.create_dataset('3BData/Raw', data=raw.T + 2048, dtype=int)
+        # d = rf.create_dataset('3BData/Raw', data=raw.T + 2048, dtype=int)
+        d = rf.create_dataset('3BData/Raw', data=((raw.T-data_range[2])/max_amp*scale).astype(int)+2048, dtype=int)
         g.attrs['Version'] = 100
         rf.create_dataset('3BRecInfo/3BRecVars/MinVolt', data=[0])
         rf.create_dataset('3BRecInfo/3BRecVars/MaxVolt', data=[1])
@@ -75,39 +93,36 @@ def openBiocamFile(filename):
     # bitDepth = recVars['BitDepth'].value[0]
     # maxV = recVars['MaxVolt'].value[0]
     # minV = recVars['MinVolt'].value[0]
-    nFrames = recVars['NRecFrames'].value[0]
-    samplingRate = recVars['SamplingRate'].value[0]
-    signalInv = recVars['SignalInversion'].value[0]
+    nFrames = recVars['NRecFrames'][0]
+    samplingRate = recVars['SamplingRate'][0]
+    signalInv = recVars['SignalInversion'][0]
 
     # Read chip variables
     chipVars = rf.require_group('3BRecInfo/3BMeaChip/')
-    nCols = chipVars['NCols'].value[0]
+    nCols = chipVars['NCols'][0]
 
     # Get the actual number of channels used in the recording
     file_format = rf['3BData'].attrs.get('Version')
     if file_format == 100:
         nRecCh = len(rf['3BData/Raw'][0])
-        # raise Warning('This may go wrong!')
     elif file_format == 101:
         nRecCh = int(1. * rf['3BData/Raw'].shape[0] / nFrames)
     else:
         raise Exception('Unknown data file format.')
 
     print('# 3Brain data format:', file_format, 'signal inversion', signalInv)
-    print('#       signal range: ', recVars['MinVolt'].value[0], '- ',
-          recVars['MaxVolt'].value[0])
-    # Compute indices
-    rawIndices = rf['3BRecInfo/3BMeaStreams/Raw/Chs'].value
-
-    # Name channels ([0..4095] for fullarray files)
-    chIndices = [(x - 1) + (y - 1) * nCols for (y, x) in rawIndices]
-    # chIndices = [(x-1) + (y-1)*nCols for (x,y) in rawIndices]
-    # Swap X and Y (old format)
+    print('#       signal range: ', recVars['MinVolt'][0], '- ',
+          recVars['MaxVolt'][0])
+    # Compute channel locations
+    r = rf['3BRecInfo/3BMeaStreams/Raw/Chs'][()]['Row']
+    c = rf['3BRecInfo/3BMeaStreams/Raw/Chs'][()]['Col']
+    rawIndices = np.vstack((r, c)).T
+    # assign channel numbers
+    chIndices = np.array([(x - 1) + (y - 1) * nCols for (y, x) in rawIndices])
 
     # determine correct function to read data
-    print("# Signal inversion looks like " + str(signalInv) + ", guessing the "
-                                                              "right method for data access.\n# If your results "
-                                                              "look strange, signal polarity is wrong.")
+    print("# Signal inversion looks like " + str(signalInv) + ", guessing correct method for data access.")
+    print("# If your results look wrong, signal polarity is may be wrong.")
     if file_format == 100:
         if signalInv == -1:
             read_function = readHDF5t_100
@@ -123,12 +138,10 @@ def openBiocamFile(filename):
 
 
 def readHDF5(rf, t0, t1):
-    """In order to use the algorithms designed for the old format, the input data must be inverted."""
     return 4095 - rf['3BData/Raw'][t0:t1].flatten().astype(ctypes.c_short)
 
 
 def readHDF5t_100(rf, t0, t1, nch):
-    """Transposed version for the interpolation method."""
     if t0 <= t1:
         d = 2048 - rf['3BData/Raw'][t0:t1].flatten('C').astype(ctypes.c_short)
         d[np.where(np.abs(d) > 1500)[0]] = 0
@@ -140,7 +153,6 @@ def readHDF5t_100(rf, t0, t1, nch):
 
 
 def readHDF5t_100_i(rf, t0, t1, nch):
-    ''' Transposed version for the interpolation method. '''
     if t0 <= t1:
         d = rf['3BData/Raw'][t0:t1].flatten('C').astype(ctypes.c_short) - 2048
         d[np.where(np.abs(d) > 1500)[0]] = 0
@@ -152,7 +164,6 @@ def readHDF5t_100_i(rf, t0, t1, nch):
 
 
 def readHDF5t_101(rf, t0, t1, nch):
-    ''' Transposed version for the interpolation method. '''
     if t0 <= t1:
         d = rf['3BData/Raw'][nch * t0:nch * t1].reshape(
             (-1, nch), order='C').flatten('C').astype(ctypes.c_short) - 2048
@@ -167,7 +178,6 @@ def readHDF5t_101(rf, t0, t1, nch):
 
 
 def readHDF5t_101_i(rf, t0, t1, nch):
-    ''' Transposed version for the interpolation method. '''
     if t0 <= t1:
         d = 2048 - rf['3BData/Raw'][nch * t0:nch * t1].reshape(
             (-1, nch), order='C').flatten('C').astype(ctypes.c_short)
