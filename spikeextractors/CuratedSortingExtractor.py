12720c18f7f8567f90c5e04ff46e8db3fece8ea0
colehurwitz <s1727843@raikonnen.inf.ed.ac.uk>
2019-03-19 16:43:59 +0000

    New Curated Sorting Extractor (cleaner with different base implementation)

--- spikeextractors/CuratedSortingExtractor.py
+++ spikeextractors/CuratedSortingExtractor.py
@@ -11,17 +11,12 @@ class CuratedSortingExtractor(SortingExtractor):
         self._parent_sorting = parent_sorting
         self._original_unit_ids = list(np.copy(parent_sorting.getUnitIds()))
         self._all_ids = list(np.copy(parent_sorting.getUnitIds()))
-        original_spike_indices = []
-        for unit_id in self._original_unit_ids:
-            original_spike_indices.append(np.asarray(list(range(len(parent_sorting.getUnitSpikeTrain(unit_id))))))
-        self._indices_dict = {}
-        for i, unit_id in enumerate(self._original_unit_ids):
-            self._indices_dict[unit_id] = original_spike_indices[i]
-        del original_spike_indices
 
+        #Create and store roots with original unit ids and cached spiketrains
         self._roots = []
         for i, unit_id in enumerate(self._original_unit_ids):
             root = Unit(unit_id)
+            root.set_spike_train(parent_sorting.getUnitSpikeTrain(unit_id))
             self._roots.append(root)
 
         self.copyUnitProperties(parent_sorting)
@@ -38,38 +33,19 @@ class CuratedSortingExtractor(SortingExtractor):
             start_frame = 0
         if end_frame is None:
             end_frame = np.Inf
+
         valid_unit_id = False
         spike_train = np.asarray([])
         for root in self._roots:
             if(root.unit_id == unit_id):
                 valid_unit_id = True
-                self._collectSpikeTrains(root)
-                inds = np.where((start_frame <= root.spike_train) & (root.spike_train < end_frame))
-                spike_train = root.spike_train[inds]
-                self._resetSpikeTrains(root)
+                full_spike_train = root.get_spike_train()
+                inds = np.where((start_frame <= full_spike_train) & (full_spike_train < end_frame))
+                spike_train = full_spike_train[inds]
         if(valid_unit_id):
             return spike_train
         else:
-            raise ValueError("invalid unit id")
-
-    def _collectSpikeTrains(self, unit): #post order traversal
-        for child in unit.get_children():
-            self._collectSpikeTrains(child)
-        if(len(unit.get_children()) == 0):
-            unit.spike_train = self._parent_sorting.getUnitSpikeTrain(unit.unit_id)
-        else:
-            new_spike_train = []
-            for child in unit.get_children():
-                new_spike_train.append(child.spike_train)
-            new_spike_train = np.asarray(new_spike_train)
-            new_spike_train = np.sort(np.concatenate(new_spike_train).ravel())
-            new_spike_train = new_spike_train[self._indices_dict[unit.unit_id]]
-            unit.spike_train = new_spike_train
-
-    def _resetSpikeTrains(self, unit): #post order traversal
-        for child in unit.get_children():
-            self._resetSpikeTrains(child)
-        unit.spike_train = np.asarray([])
+            raise ValueError(str(unit_id) + " is an invalid unit id")
 
     def printCurationTree(self, unit_id):
         '''This function prints the current curation tree for the unit_id (roots are current unit ids).
@@ -108,10 +84,9 @@ class CuratedSortingExtractor(SortingExtractor):
             for unit_id in unit_ids:
                 root_index = root_ids.index(unit_id)
                 indices_to_be_deleted.append(root_index)
-                del self._indices_dict[unit_id]
             self._roots = [self._roots[i] for i,_ in enumerate(root_ids) if i not in indices_to_be_deleted]
         else:
-            raise ValueError("invalid unit ids")
+            raise ValueError(str(unit_ids) + " has one or more invalid unit ids")
 
     def mergeUnits(self, unit_ids):
         '''This function merges two roots from the curation tree according to the given unit_ids. It creates a new unit_id and root
@@ -126,24 +101,26 @@ class CuratedSortingExtractor(SortingExtractor):
         for i in range(len(self._roots)):
             root_id = self._roots[i].unit_id
             root_ids.append(root_id)
+
+        indices_to_be_deleted = []
         if(set(unit_ids).issubset(set(root_ids)) and len(unit_ids) > 1):
             new_root_id = max(self._all_ids)+1
             self._all_ids.append(new_root_id)
             new_root = Unit(new_root_id)
-            indices_to_be_deleted = []
-            self._indices_dict[new_root_id] = []
+            all_spike_trains = []
             for unit_id in unit_ids:
                 root_index = root_ids.index(unit_id)
                 new_root.add_child(self._roots[root_index])
-                for spike_index in self._indices_dict[unit_id]:
-                    self._indices_dict[new_root_id].append(len(self._indices_dict[new_root_id]))
-
+                all_spike_trains.append(self._roots[root_index].get_spike_train())
+                self._roots[root_index].set_spike_train(np.asarray([])) #clear spiketrain
                 indices_to_be_deleted.append(root_index)
-            self._indices_dict[new_root_id] = np.asarray(self._indices_dict[new_root_id])
+
+            new_root.set_spike_train(np.asarray(np.sort(np.concatenate(all_spike_trains))))
+            del all_spike_trains
             self._roots = [self._roots[i] for i,_ in enumerate(root_ids) if i not in indices_to_be_deleted]
             self._roots.append(new_root)
         else:
-            raise ValueError("invalid unit ids")
+            raise ValueError(str(unit_ids) + " has one or more invalid unit ids")
 
     def splitUnit(self, unit_id, indices):
         '''This function splits a root from the curation tree according to the given unit_id and indices. It creates two new unit_ids
@@ -162,31 +139,38 @@ class CuratedSortingExtractor(SortingExtractor):
             root_ids.append(root_id)
 
         if(unit_id in root_ids):
+            indices_1 = np.sort(np.asarray(list(set(indices))))
+
             root_index = root_ids.index(unit_id)
-            root_spike_indices = self._indices_dict[unit_id]
-            if(set(indices).issubset(set(root_spike_indices)) and len(indices) > 0 and len(indices) < len(root_spike_indices)):
-                new_child = self._roots[root_index]
+            new_child = self._roots[root_index]
+            original_spike_train = self._roots[root_index].get_spike_train()
 
-                new_root_1_id = max(self._all_ids)+1
-                self._all_ids.append(new_root_1_id)
-                new_root_1 = Unit(new_root_1_id)
-                new_root_1.add_child(new_child)
-                self._indices_dict[new_root_1_id] = np.asarray(indices)
+            try:
+                spike_train_1 = original_spike_train[indices_1]
+            except IndexError:
+                print(str(indices) + " out of bounds for the spike train of " + str(unit_id))
 
-                non_indices = [index for i, index in enumerate(root_spike_indices) if i not in indices]
-                new_root_2_id = max(self._all_ids)+1
-                self._all_ids.append(new_root_2_id)
-                new_root_2 = Unit(new_root_2_id)
-                new_root_2.add_child(new_child)
-                self._indices_dict[new_root_2_id] = np.asarray(non_indices)
+            indices_2 = list(set(range(len(original_spike_train))) - set(indices_1))
+            spike_train_2 = original_spike_train[indices_2]
+            del original_spike_train
 
-                del self._roots[root_index]
-                self._roots.append(new_root_1)
-                self._roots.append(new_root_2)
-            else:
-                raise ValueError("invalid indices")
+            new_root_1_id = max(self._all_ids)+1
+            self._all_ids.append(new_root_1_id)
+            new_root_1 = Unit(new_root_1_id)
+            new_root_1.add_child(new_child)
+            new_root_1.set_spike_train(spike_train_1)
+
+            new_root_2_id = max(self._all_ids)+1
+            self._all_ids.append(new_root_2_id)
+            new_root_2 = Unit(new_root_2_id)
+            new_root_2.add_child(new_child)
+            new_root_2.set_spike_train(spike_train_2)
+
+            del self._roots[root_index]
+            self._roots.append(new_root_1)
+            self._roots.append(new_root_2)
         else:
-            raise ValueError("invalid unit ids")
+            raise ValueError(str(unit_id) + " non-valid unit id")
 
 
 # The Unit class is a node in the curation tree. Each Unit contains its unit_id, children, and spike_train.
@@ -195,6 +179,11 @@ class Unit(object):
         self.unit_id = unit_id
         self.children = []
         self.spike_train = np.asarray([])
+
+    def set_spike_train(self, spike_train):
+        self.spike_train = spike_train
+
+    def get_spike_train(self):        return self.spike_train
 
     def add_child(self, child):
         self.children.append(child)
