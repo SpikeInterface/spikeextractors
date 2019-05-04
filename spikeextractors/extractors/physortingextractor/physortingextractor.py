07b9f467cb5505c3ff1452e5ef2289117f3462a8
Alessio Buccino <alejoe9187@gmail.com>
2019-05-04 18:56:52 +0200

    added seed to toy example

--- spikeextractors/extractors/physortingextractor/physortingextractor.py
+++ spikeextractors/extractors/physortingextractor/physortingextractor.py
@@ -38,6 +38,7 @@ class PhySortingExtractor(SortingExtractor):
             self.set_unit_spike_features(clust, 'pc_features', pc_features[idx])
 
         # set unit quality properties
+        print(phy_folder)
         if (phy_folder / 'cluster_groups.csv').is_file():
             with open(phy_folder / 'cluster_groups.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
@@ -49,6 +50,20 @@ class PhySortingExtractor(SortingExtractor):
                         tokens = row[0].split("\t")
                         self.set_unit_property(int(tokens[0]), 'quality', tokens[1])
                         line_count += 1
+        elif (phy_folder / 'cluster_group.tsv').is_file():
+            with open(phy_folder / 'cluster_group.tsv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter='\t')
+                line_count = 0
+                for row in csv_reader:
+                    if line_count == 0:
+                        line_count += 1
+                    else:
+                        self.set_unit_property(int(row[0]), 'quality', row[1])
+                        line_count += 1
+        if 'quality' in self.get_unit_property_names():
+            for unit in self.get_unit_ids():
+                if 'quality' not in self.get_unit_property_names(unit):
+                    self.set_unit_property(unit, 'quality', 'unsorted')
 
     def get_unit_ids(self):
         return list(self._unit_ids)
