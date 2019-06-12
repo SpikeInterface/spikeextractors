fd46ea245a74c76807eb047fbf5f77573684835a
colehurwitz <colehurwitz@gmail.com>
2019-06-12 15:00:52 +0100

    Corrected npz test

--- tests/test_npzsortingextractor.py
+++ tests/test_npzsortingextractor.py
@@ -16,13 +16,15 @@ class TestNpzSortingExtractors(unittest.TestCase):
 
         recording, sorting_gt = se.example_datasets.toy_example(num_channels=4, duration=10, seed=0)
         print(sorting_gt)
-        
+
         se.NpzSortingExtractor.write_sorting(sorting_gt, 'test_NpzSortingExtractors.npz')
 
         npz = np.load('test_NpzSortingExtractors.npz')
+        sorting_npz = se.NpzSortingExtractor('test_NpzSortingExtractors.npz')
         units_ids = npz['unit_ids']
         self.assertEqual(list(units_ids), list(sorting_gt.get_unit_ids()))
-        self.assertEqual(npz.get_sampling_frequency(), 30000.0)
+        self.assertEqual(list(sorting_npz.get_unit_ids()), list(sorting_gt.get_unit_ids()))
+        self.assertEqual(sorting_npz.get_sampling_frequency(), 30000.0)
 
     def test_empty_write(self):
         sorting_empty = se.NumpySortingExtractor()
