67683405954d3e629138ceb7342b8edf73357e57
Alessio Buccino <alejoe9187@gmail.com>
2019-09-24 21:12:30 +0200

    added destructor

--- spikeextractors/cacherecordingextractor.py
+++ spikeextractors/cacherecordingextractor.py
@@ -1,5 +1,6 @@
 from spikeextractors.extractors.bindatrecordingextractor import BinDatRecordingExtractor
 import tempfile
+import os
 
 
 class CacheRecordingExtractor(BinDatRecordingExtractor):
@@ -7,12 +8,15 @@ class CacheRecordingExtractor(BinDatRecordingExtractor):
     extractor_name = 'CacheRecordingExtractor'
 
     def __init__(self, recording, dtype=None, chunksize=None):
-        tmp_file = tempfile.NamedTemporaryFile(suffix=".dat").name
+        self._tmp_file = tempfile.NamedTemporaryFile(suffix=".dat").name
         if dtype is None:
             dtype = recording.get_traces(start_frame=0, end_frame=2).dtype
-        recording.write_to_binary_dat_format(save_path=tmp_file, dtype=dtype, chunksize=chunksize)
-        BinDatRecordingExtractor.__init__(self, tmp_file, numchan=recording.get_num_channels(),
+        recording.write_to_binary_dat_format(save_path=self._tmp_file, dtype=dtype, chunksize=chunksize)
+        BinDatRecordingExtractor.__init__(self, self._tmp_file, numchan=recording.get_num_channels(),
                                           recording_channels=recording.get_channel_ids(),
                                           sampling_frequency=recording.get_sampling_frequency(),
                                           dtype=dtype)
         self.copy_channel_properties(recording)
+
+    def __del__(self):
+        os.remove(self._tmp_file)
