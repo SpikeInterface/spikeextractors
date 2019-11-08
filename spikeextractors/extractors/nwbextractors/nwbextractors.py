23db87f2dc83e9ae9bf36acd99cdc98dd0551927
luiztauffer <luiz_tauffer@hotmail.com>
2019-11-08 21:24:28 +0700

    write epochs to nwb file

--- spikeextractors/extractors/nwbextractors/nwbextractors.py
+++ spikeextractors/extractors/nwbextractors/nwbextractors.py
@@ -55,6 +55,26 @@ def get_dynamic_table_property(dynamic_table, *, row_ids=None, property_name):
     return [dynamic_table[property_name][all_row_ids.index(x)] for x in row_ids]
 
 
+
+def most_relevant_ch(traces):
+    """
+    Calculates the most relevant channel for an Unit.
+    Estimates the channel where the max-min difference of the average traces is greatest.
+
+    traces : ndarray
+        ndarray of shape (nSpikes, nChannels, nSamples)
+    """
+    n_channels = traces.shape[1]
+    avg = np.mean(traces, axis=0)
+
+    max_min = np.zeros(n_channels)
+    for ch in range(n_channels):
+        max_min[ch] = avg[ch, :].max() - avg[ch, :].min()
+
+    relevant_ch = np.argmax(max_min)
+    return relevant_ch
+
+
 class NwbRecordingExtractor(se.RecordingExtractor):
     extractor_name = 'NwbRecordingExtractor'
     has_default_locations = True
@@ -169,84 +189,6 @@ class NwbRecordingExtractor(se.RecordingExtractor):
     def get_channel_ids(self):
         return self.channel_ids.tolist()
 
-    # def set_channel_locations(self, channel_ids=None, locations=None):
-    #     with NWBHDF5IO(self._path, 'r+') as io:
-    #         nwbfile = io.read()
-    #         set_dynamic_table_property(nwbfile.electrodes, channel_ids, 'x', locations[:, 0])
-    #         set_dynamic_table_property(nwbfile.electrodes, channel_ids, 'y', locations[:, 1])
-    #         set_dynamic_table_property(nwbfile.electrodes, channel_ids, 'z', locations[:, 2])
-    #
-    # def set_channel_groups(self, channel_ids=None, groups=None):
-    #     # todo
-    #     raise NotImplementedError()
-    #
-    # def get_channel_groups(self, channel_ids=None):
-    #     with NWBHDF5IO(self._path, 'r') as io:
-    #         nwbfile = io.read()
-    #         return get_dynamic_table_property(nwbfile.electrodes, row_ids=channel_ids, property_name='group_name')
-    #
-    # def set_channel_gains(self, channel_ids, gains):
-    #     self.set_channels_property(channel_ids=channel_ids,
-    #                                property_name='gain',
-    #                                values=gains,
-    #                                default_value=1.)
-    #
-    # def set_channel_property(self, channel_id, property_name=None, value=None, default_value=np.nan,
-    #                          description='no description'):
-    #     self.set_channels_property(channel_ids=[channel_id], property_name=property_name, values=[value],
-    #                                default_value=default_value, description=description)
-    #
-    # def set_channels_property(self, channel_ids, property_name, values, default_value=np.nan,
-    #                           description='no description'):
-    #     with NWBHDF5IO(self._path, 'r+') as io:
-    #         nwbfile = io.read()
-    #         set_dynamic_table_property(nwbfile.electrodes, row_ids=channel_ids, property_name=property_name,
-    #                                    values=values, default_value=default_value, description=description)
-    #         es = nwbfile.acquisition[self._electrical_series_name]
-    #         self.electrodes_df = es.electrodes.table.to_dataframe()
-    #         io.write(nwbfile)
-    #
-    # def get_channel_property(self, channel_id, property_name):
-    #     return self.electrodes_df[property_name][channel_id]
-    #
-    # def get_channel_property_names(self, channel_id=None):
-    #     return list(self.electrodes_df.columns)
-    #
-    # def get_shared_channel_property_names(self, channel_ids=None):
-    #     return list(self.electrodes_df.columns)
-    #
-    # def copy_channel_properties(self, recording, channel_ids=None, default_values=None):
-    #     if channel_ids is None:
-    #         channel_ids = recording.get_channel_ids()
-    #     else:
-    #         if not isinstance(channel_ids, list) or not all(isinstance(x, int) for x in channel_ids):
-    #             raise ValueError("'channel_ids' must be a list of integers")
-    #         existing_ids = self.get_channel_ids()
-    #         if any(x not in existing_ids for x in channel_ids):
-    #             raise ValueError("'channel_ids' contains values outside the range of existing ids")
-    #
-    #     new_property_names = recording.get_shared_channel_property_names()
-    #     curr_property_names = self.get_shared_channel_property_names()
-    #     if default_values is None:
-    #         default_values = [np.nan] * len(new_property_names)
-    #     else:
-    #         if len(default_values) != len(new_property_names):
-    #             raise ValueError("'default_values' list must have length equal to" +
-    #                             " number of properties to be copied.")
-    #
-    #     # Copies only properties that do not exist already in NWB file
-    #     for i, pr in enumerate(new_property_names):
-    #         if pr in curr_property_names:
-    #             # todo
-    #             raise NotImplementedError()
-    #         else:
-    #             for channel_id in channel_ids:
-    #                 pr_value = recording.get_channel_property(channel_id=channel_id,
-    #                                                           property_name=pr)
-    #                 self.set_channel_property(channel_id=channel_id,
-    #                                           property_name=pr,
-    #                                           value=pr_value)
-    #
     # def add_epoch(self, epoch_name, start_frame, end_frame):
     #     check_nwb_install()
     #     if not isinstance(epoch_name, str):
@@ -416,6 +358,26 @@ class NwbRecordingExtractor(se.RecordingExtractor):
                     description='acquisition_description'
                 )
                 nwbfile.add_acquisition(ephys_ts)
+
+            # add/update epochs
+            for (name, ep) in recording._epochs.items():
+                if nwbfile.epochs is None:
+                    nwbfile.add_epoch(
+                        start_time=recording.frame_to_time(ep['start_frame']),
+                        stop_time=recording.frame_to_time(ep['end_frame']),
+                        tags=name
+                    )
+                else:
+                    if [name] in nwbfile.epochs['tags'][:]:
+                        ind = nwbfile.epochs['tags'][:].index([name])
+                        nwbfile.epochs['start_time'].data[ind] = recording.frame_to_time(ep['start_frame'])
+                        nwbfile.epochs['stop_time'].data[ind] = recording.frame_to_time(ep['end_frame'])
+                    else:
+                        nwbfile.add_epoch(
+                            start_time=recording.frame_to_time(ep['start_frame']),
+                            stop_time=recording.frame_to_time(ep['end_frame']),
+                            tags=name
+                        )
 
             io.write(nwbfile)
 
@@ -950,22 +912,3 @@ class NwbSortingExtractor(se.SortingExtractor):
                     nwbfile.add_unit(id=id, spike_times=spkt)
 
             io.write(nwbfile)
-
-
-def most_relevant_ch(traces):
-    """
-    Calculates the most relevant channel for an Unit.
-    Estimates the channel where the max-min difference of the average traces is greatest.
-
-    traces : ndarray
-        ndarray of shape (nSpikes, nChannels, nSamples)
-    """
-    n_channels = traces.shape[1]
-    avg = np.mean(traces, axis=0)
-
-    max_min = np.zeros(n_channels)
-    for ch in range(n_channels):
-        max_min[ch] = avg[ch, :].max() - avg[ch, :].min()
-
-    relevant_ch = np.argmax(max_min)
-    return relevant_ch
