a0bd1cca108a343ff10729a9103dc0a02a939ba9
Alessio Buccino <alejoe9187@gmail.com>
2019-11-27 14:21:59 +0100

    Fix mcsh5 channel order bug

--- spikeextractors/extractors/mcsh5recordingextractor/mcsh5recordingextractor.py
+++ spikeextractors/extractors/mcsh5recordingextractor/mcsh5recordingextractor.py
@@ -71,22 +71,30 @@ class MCSH5RecordingExtractor(RecordingExtractor):
         if end_frame is None:
             end_frame = self.get_num_frames()
         if channel_ids is None:
-            channel_idxs = self._channel_ids
+            channel_ids = self._channel_ids
         else:
             if isinstance(channel_ids, (int, np.integer)):
                 assert channel_ids in self._channel_ids, 'channel_id {} not found'.format(channel_ids)
-                channel_idxs = np.where(self._channel_ids == channel_ids)[0][0]
+                channel_ids = np.where(self._channel_ids == channel_ids)[0][0]
             else:
-                channel_idxs = []
+                channel_ids = []
                 for m in channel_ids:
                     assert m in self._channel_ids, 'channel_id {} not found'.format(m)
-                    channel_idxs.append(np.where(self._channel_ids == m)[0][0])
+                    channel_ids.append(np.where(self._channel_ids == m)[0][0])
 
         stream = self._rf.require_group('/Data/Recording_0/AnalogStream/Stream_' + str(self._stream_id))
-        data_v = np.array(stream.get('ChannelData'), dtype=np.int) * self._convFact.astype(float) * \
-                 (10.0 ** self._exponent)
+        conv = self._convFact.astype(float) * (10.0 ** self._exponent)
+        
+        if np.array(channel_ids).size > 1:
+            if np.any(np.diff(channel_ids) < 0):
+                sorted_idx = np.argsort(channel_ids)
+                recordings = stream.get('ChannelData')[np.sort(channel_ids), start_frame:end_frame]
+                return recordings[sorted_idx] * conv
+            else:
+                return stream.get('ChannelData')[np.sort(channel_ids), start_frame:end_frame] * conv
+        else:
+            return stream.get('ChannelData')[np.array(channel_ids), start_frame:end_frame] * conv
 
-        return data_v[channel_idxs, start_frame:end_frame]
 
     @staticmethod
     def write_recording(recording, save_path):
