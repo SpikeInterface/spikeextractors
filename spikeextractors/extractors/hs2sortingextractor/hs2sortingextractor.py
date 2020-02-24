fix load units HS

spikeextractors/extractors/hs2sortingextractor/hs2sortingextractor.py
@@ -42,9 +42,9 @@ class HS2SortingExtractor(SortingExtractor):
             self._unit_locs = self._rf['centres'][()]  # cache for faster access
             if self._unit_locs.shape[0] < 5:  # check if old, transposed format
                 self._unit_locs = self._unit_locs.T
-            for unit_id in self._unit_ids:
+            for u_i, unit_id in enumerate(self._unit_ids):
                 self._unit_properties[unit_id] = {}
-                self._unit_properties[unit_id]['unit_location'] = self._unit_locs[unit_id]
+                self._unit_properties[unit_id]['unit_location'] = self._unit_locs[u_i]
         inds = []  # get these only once
         for unit_id in self._unit_ids:
             inds.append(np.where(self._cluster_id==unit_id)[0])