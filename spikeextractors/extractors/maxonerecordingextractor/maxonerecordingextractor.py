336ed631917ae88a674e1ef2a03c3edb210f851e
Alessio Buccino <alejoe9187@gmail.com>
2020-10-07 18:52:58 +0200

    Load activity map (spike counts) and fix locations in multi channel recording extractor

--- spikeextractors/extractors/maxonerecordingextractor/maxonerecordingextractor.py
+++ spikeextractors/extractors/maxonerecordingextractor/maxonerecordingextractor.py
@@ -43,16 +43,40 @@ class MaxOneRecordingExtractor(RecordingExtractor):
         # remove unused channels
         routed_idxs = np.where(electrodes > -1)[0]
         self._channel_ids = list(channels[routed_idxs])
+        self._electrode_ids = list(electrodes[routed_idxs])
         self._num_channels = len(self._channel_ids)
         self._fs = float(20000)
         self._signals = self._filehandle['sig']
         self._num_frames = self._signals.shape[1]
 
-        for i_ch, ch in zip(routed_idxs, self._channel_ids):
+        for i_ch, ch, el in zip(routed_idxs, self._channel_ids, self._electrode_ids):
             self.set_channel_locations([self._mapping['x'][i_ch], self._mapping['y'][i_ch]], ch)
+            self.set_channel_property(ch, 'electrode', el)
+
+        if 'proc0' in self._filehandle:
+            if 'spikeTimes' in self._filehandle['proc0']:
+                spikes = self._filehandle['proc0']['spikeTimes']
+
+                spike_mask = [True] * len(spikes)
+                for i, ch in enumerate(spikes['channel']):
+                    if ch not in self._channel_ids:
+                        spike_mask[i] = False
+                spikes_channels = np.array(spikes['channel'])[spike_mask]
+
+                # load activity as property
+                activity_channels, counts = np.unique(spikes_channels, return_counts=True)
+                activity_channels = list(activity_channels)
+                for ch in self.get_channel_ids():
+                    if ch in activity_channels:
+                        self.set_channel_property(ch, 'activity', counts[activity_channels.index(ch)])
+                    else:
+                        self.set_channel_property(ch, 'activity', 0)
 
     def get_channel_ids(self):
         return list(self._channel_ids)
+
+    def get_electrode_ids(self):
+        return list(self._electrode_ids)
 
     def get_num_frames(self):
         return self._num_frames
