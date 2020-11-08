4a6675ac4c8451fbb1a7c0f80c959d55333c466d
Cody Baker <cbaker9@nd.edu>
2020-11-08 14:03:00 -0500

    further hotfix to the adding of certain custom columns in nwbrecordingextractor.write_recording()

--- spikeextractors/extractors/nwbextractors/nwbextractors.py
+++ spikeextractors/extractors/nwbextractors/nwbextractors.py
@@ -486,7 +486,8 @@ class NwbRecordingExtractor(se.RecordingExtractor):
         else:
             nwb_elec_ids = nwbfile.electrodes.id.data[:]
         for metadata_column in metadata['Ecephys']['Electrodes']:
-            if nwbfile.electrodes is None or metadata_column['name'] not in nwbfile.electrodes.colnames:
+            if (nwbfile.electrodes is None or metadata_column['name'] not in nwbfile.electrodes.colnames) \
+                    and metadata_column['name'] != 'group_name':
                 nwbfile.add_electrode_column(str(metadata_column['name']),
                                              str(metadata_column['description']))
 
