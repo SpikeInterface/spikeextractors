2de169b00c256683a383737a2ef9d954a2fd7f11
Alessio Buccino <alejoe9187@gmail.com>
2020-11-24 19:03:36 +0100

    Implemented Blackrock NEO extractors

--- spikeextractors/extractors/neoextractors/__init__.py
+++ spikeextractors/extractors/neoextractors/__init__.py
@@ -1,3 +1,4 @@
 from .plexonextractor import PlexonRecordingExtractor, PlexonSortingExtractor
 from .neuralynxextractor import NeuralynxRecordingExtractor, NeuralynxSortingExtractor
 from .mcsrawrecordingextractor import MCSRawRecordingExtractor
+from .blackrockextractor import BlackrockRecordingExtractor, BlackrockSortingExtractor
