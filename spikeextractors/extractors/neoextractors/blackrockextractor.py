a256fb7bc4484bdfef9adb465f50e7f7a20eccac
luiz <luiz_tauffer@hotmail.com>
2021-01-13 21:21:12 +0100

    kwargs

--- spikeextractors/extractors/neoextractors/blackrockextractor.py
+++ spikeextractors/extractors/neoextractors/blackrockextractor.py
@@ -32,9 +32,10 @@ class BlackrockRecordingExtractor(NeoBaseRecordingExtractor):
     NeoRawIOClass = 'BlackrockRawIO'
 
     def __init__(self, filename: PathType, nsx_to_load: Optional[int] = None,
-                 block_index: Optional[float] = None, seg_index: Optional[float] = None):
+                 block_index: Optional[float] = None, seg_index: Optional[float] = None,
+                 **kwargs):
         super().__init__(filename=filename, nsx_to_load=nsx_to_load, 
-                         block_index=block_index, seg_index=seg_index)
+                         block_index=block_index, seg_index=seg_index, **kwargs)
 
 
 class BlackrockSortingExtractor(NeoBaseSortingExtractor):
