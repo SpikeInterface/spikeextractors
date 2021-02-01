994086972b1d871a2f7ddda568c6fdbb48c985fa
Cody Baker <cbaker9@nd.edu>
2021-02-01 15:10:53 -0500

    updated gin test to mimic roiextractors method

--- tests/test_gin_repo.py
+++ tests/test_gin_repo.py
@@ -1,6 +1,8 @@
 import unittest
+from pathlib import Path
 
 from datalad.api import install
+from parameterized import parameterized
 
 from spikeextractors import NwbRecordingExtractor, NeuralynxRecordingExtractor
 
@@ -9,6 +11,42 @@ class TestNwbConversions(unittest.TestCase):
 
     def setUp(self):
         self.dataset = install('https://gin.g-node.org/NeuralEnsemble/ephy_testing_data')
+
+    def get_data(self, rt_write_fname, rt_read_fname, save_fname, dataset_path):
+        if rt_read_fname is None:
+            rt_read_fname = rt_write_fname
+        save_path = self.savedir/save_fname
+        rt_write_path = self.savedir/rt_write_fname
+        rt_read_path = self.savedir/rt_read_fname
+        resp = self.dataset.get(dataset_path)
+
+        return rt_write_path, rt_read_path, save_path
+
+    @parameterized.expand([
+        (
+            NeuralynxRecordingExtractor,
+            'neuralynx/Cheetah_v1.1.0/orginial_data/CSC67_trunc.Ncs',
+            'neuralynx/Cheetah_v1.1.0/orginial_data/CSC67_trunc.Ncs',
+            'neuralynx_test.nwb',
+            'neuralynx_test.hdf5'
+        )
+    ])
+    def test_convert_recording_extractor_to_nwb(
+        self, se_class, dataset_path, dataset_path_arg, save_fname, rt_write_fname, rt_read_fname=None
+    ):
+
+        rt_write_path, rt_read_path, save_path = self.get_data(rt_write_fname, rt_read_fname, save_fname, dataset_path)
+
+        path = Path.cwd()/'ephys_testing_data'/dataset_path_arg
+        re = se_class(path)
+        NwbRecordingExtractor.write_recording(re, save_path)
+        # nwb_seg_ex = NwbSegmentationExtractor(save_path)
+        # check_segmentations_equal(roi_ex, nwb_seg_ex)
+        # try:
+        #     roi_ex_class.write_segmentation(nwb_seg_ex, rt_write_path)
+        # except NotImplementedError:
+        #     return
+        # seg_ex_rt = roi_ex_class(rt_read_path)
 
     def test_convert_neuralynx(self):
         resp = self.dataset.get('neuralynx/Cheetah_v1.1.0/orginial_data/CSC67_trunc.Ncs')
