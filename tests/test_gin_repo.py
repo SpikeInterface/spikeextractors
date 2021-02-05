21c124006680bdc8988c40e992b7f3c0b066841f
Cody Baker <cbaker9@nd.edu>
2021-02-05 16:36:39 -0500

    added klustakwik sorter and sorting tests

--- tests/test_gin_repo.py
+++ tests/test_gin_repo.py
@@ -6,7 +6,7 @@ from datalad.api import install, Dataset
 from parameterized import parameterized
 
 import spikeextractors as se
-from spikeextractors.testing import check_recordings_equal
+from spikeextractors.testing import check_recordings_equal, check_sortings_equal
 
 
 class TestNwbConversions(unittest.TestCase):
@@ -32,25 +32,41 @@ class TestNwbConversions(unittest.TestCase):
             "neuroscope/test1",
             "neuroscope/test1/test1.dat"
         ),
-        (
-            se.IntanRecordingExtractor,
-            "intan",
-            "intan/intan_rhd_test_1.rhd"
-        ),
-        (
-            se.IntanRecordingExtractor,
-            "intan",
-            "intan/intan_rhd_test_1.rhs"
-        )
+        # (
+        #     se.IntanRecordingExtractor,
+        #     "intan",
+        #     "intan/intan_rhd_test_1.rhd"
+        # ),
+        # (
+        #     se.IntanRecordingExtractor,
+        #     "intan",
+        #     "intan/intan_rhd_test_1.rhs"
+        # )
     ])
     def test_convert_recording_extractor_to_nwb(self, se_class, dataset_path, se_path_arg):
         nwb_save_path = self.savedir / f"{se_class.__name__}_test.nwb"
         self.dataset.get(dataset_path)
 
-        re = se_class(Path.cwd() / "ephy_testing_data" / se_path_arg)
-        se.NwbRecordingExtractor.write_recording(re, nwb_save_path)
-        nwb_re = se.NwbRecordingExtractor(nwb_save_path)
-        check_recordings_equal(re, nwb_re)
+        recording = se_class(Path.cwd() / "ephy_testing_data" / se_path_arg)
+        se.NwbRecordingExtractor.write_recording(recording, nwb_save_path)
+        nwb_recording = se.NwbRecordingExtractor(nwb_save_path)
+        check_recordings_equal(recording, nwb_recording)
+
+    @parameterized.expand([
+        (
+            se.KlustaSortingExtractor,
+            "klustakwik/test1",
+            "klustakwik/test1"
+        )
+    ])
+    def test_convert_sorting_extractor_to_nwb(self, se_class, dataset_path, se_path_arg):
+        nwb_save_path = self.savedir / f"{se_class.__name__}_test.nwb"
+        self.dataset.get(dataset_path)
+
+        sorting = se_class(Path.cwd() / "ephy_testing_data" / se_path_arg)
+        se.NwbRecordingExtractor.write_sorting(sorting, nwb_save_path)
+        nwb_sorting = se.NwbRecordingExtractor(nwb_save_path)
+        check_sortings_equal(sorting, nwb_sorting)
 
 
 if __name__ == '__main__':
