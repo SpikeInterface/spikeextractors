f03ff028d7a008ec01606ecfd569be8e476d1c89
sbuergers <sbuergers@gmail.com>
2021-03-18 21:44:03 -0400

    first attempt adding axonaextractor

---
+++ spikeextractors/extractors/neoextractors/axonaextractor.py
@@ -0,0 +1,13 @@
+from .neobaseextractor import NeoBaseRecordingExtractor
+
+try:
+    import neo
+    HAVE_NEO = True
+except ImportError:
+    HAVE_NEO = False
+ 
+
+class AxonaRecordingExtractor(NeoBaseRecordingExtractor):
+    extractor_name='AxonaRecording'
+    mode='file'
+    NeoRawIOClass='AxonaRawIO'
