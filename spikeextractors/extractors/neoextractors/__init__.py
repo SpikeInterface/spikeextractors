0cda72030e1510bc9abdfb7474c779600da4ca6a
sbuergers <sbuergers@gmail.com>
2021-03-19 09:13:22 -0400

    resolved conflict from axonarawio and upstream master

--- spikeextractors/extractors/neoextractors/__init__.py
+++ spikeextractors/extractors/neoextractors/__init__.py
@@ -2,8 +2,5 @@ from .plexonextractor import PlexonRecordingExtractor, PlexonSortingExtractor
 from .neuralynxextractor import NeuralynxRecordingExtractor, NeuralynxSortingExtractor
 from .mcsrawrecordingextractor import MCSRawRecordingExtractor
 from .blackrockextractor import BlackrockRecordingExtractor, BlackrockSortingExtractor
-<<<<<<< HEAD
 from .axonaextractor import AxonaRecordingExtractor
-=======
 from .spikegadgetsextractor import SpikeGadgetsRecordingExtractor
->>>>>>> upstream/master
