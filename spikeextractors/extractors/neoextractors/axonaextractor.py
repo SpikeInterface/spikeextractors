d97f3c8b592418a6f33a3054f98cf1fb56e2fa88
sbuergers <sbuergers@gmail.com>
2021-04-15 15:58:49 -0400

    automatically read channel groups from neo annotations

--- spikeextractors/extractors/neoextractors/axonaextractor.py
+++ spikeextractors/extractors/neoextractors/axonaextractor.py
@@ -12,3 +12,9 @@ class AxonaRecordingExtractor(NeoBaseRecordingExtractor):
     extractor_name = 'AxonaRecording'
     mode = 'file'
     NeoRawIOClass = 'AxonaRawIO'
+
+    def __init__(self, **kargs):
+        NeoBaseRecordingExtractor.__init__(self, **kargs)
+        self._key_properties['group'] = self.neo_reader.\
+            raw_annotations['blocks'][0]['segments'][0]['signals']\
+                           [0]['__array_annotations__']['tetrode_id']
