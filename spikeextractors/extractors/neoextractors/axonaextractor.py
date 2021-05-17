977bef6cf8c5cf8a727094dc45d97118e71edd8e
sbuergers <sbuergers@gmail.com>
2021-05-17 14:28:11 -0400

    allow line length of 120 characters

--- spikeextractors/extractors/neoextractors/axonaextractor.py
+++ spikeextractors/extractors/neoextractors/axonaextractor.py
@@ -17,14 +17,11 @@ class AxonaRecordingExtractor(NeoBaseRecordingExtractor):
         super().__init__(self, **kargs)
 
         # Read channel groups by tetrode IDs
-        self.set_channel_groups(groups=[x - 1 for x in self.neo_reader.raw_annotations['blocks'][0]['segments'][0]['signals'][0]['__array_annotations__']['tetrode_id']])
+        self.set_channel_groups(groups=[x - 1 for x in self.neo_reader.raw_annotations[
+            'blocks'][0]['segments'][0]['signals'][0]['__array_annotations__']['tetrode_id']])
 
-
-        header_channels = self.neo_reader\
-                              .header['signal_channels'][slice(None)]
-
+        header_channels = self.neo_reader.header['signal_channels'][slice(None)]
 
         names = header_channels['name']
         for i, ind in enumerate(self.get_channel_ids()):
-            self.set_channel_property(channel_id=ind, property_name='name',
-                                      value=names[i])
+            self.set_channel_property(channel_id=ind, property_name='name', value=names[i])
